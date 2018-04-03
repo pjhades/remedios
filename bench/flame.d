0495a2525cdc8e720f843e274b64c378f6608c82
Jing Peng <pj.hades@gmail.com>
2018-04-03 10:36:31 -0400

    Lower callstack sampling rate

--- bench/flame.d
+++ bench/flame.d
@@ -1,6 +1,6 @@
 #!/usr/bin/env dtrace -s
 /* sudo ./flame.d -c ./target/release/bench | stackcollapse.pl | flamegraph.pl >out.svg */
-profile-1ms
+profile-100
 /pid == $target && arg1/
 {
     @[ustack(100)] = count();
